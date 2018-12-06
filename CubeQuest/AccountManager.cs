307a55c627beb9c42dea83fdba2db7bbb40d42e9
kraxarn <me@kraxarn.com>
2018-12-06 22:29:52 +0100

    Add ability to login

---
+++ CubeQuest/AccountManager.cs
@@ -0,0 +1,66 @@
+﻿using Android.Content;
+using Android.Gms.Auth.Api;
+using Android.Gms.Auth.Api.SignIn;
+using Android.Gms.Common;
+using Android.Gms.Common.Apis;
+using System;
+using AlertDialog = Android.Support.V7.App.AlertDialog;
+
+namespace CubeQuest
+{
+	public class AccountManager : Java.Lang.Object, GoogleApiClient.IOnConnectionFailedListener
+	{
+		private readonly GoogleApiClient googleClient;
+
+		private readonly MainActivity mainActivity;
+
+		public AccountManager(MainActivity activity)
+		{
+			mainActivity = activity;
+            
+			var signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DefaultGamesSignIn)
+				.Build();
+
+			googleClient = new GoogleApiClient.Builder(activity)
+				.EnableAutoManage(activity, this)
+				.AddApi(Auth.GOOGLE_SIGN_IN_API, signInOptions)
+				.Build();
+		}
+
+		public void OnConnectionFailed(ConnectionResult result)
+		{
+			// TODO
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Get intent used to sign in with Google
+		/// </summary>
+		public Intent GetSignInIntent() => 
+			Auth.GoogleSignInApi.GetSignInIntent(googleClient);
+
+		/// <summary>
+		/// Handle Intent result
+		/// (called from OnActivityResult)
+		/// </summary>
+		public void HandleResult(Intent data)
+		{
+			// Result from intent GoogleSignInApi.GetSignInIntent()
+			HandleSignInResult(Auth.GoogleSignInApi.GetSignInResultFromIntent(data));
+		}
+
+		private void HandleSignInResult(GoogleSignInResult result)
+		{
+			// Show game if successful
+			// TODO
+
+			new AlertDialog.Builder(mainActivity)
+				.SetTitle(result.IsSuccess ? "Signed in" : "Sign in error")
+				.SetMessage(result.IsSuccess ? 
+					"You are now signed in, good for you" : 
+					"Couldn't sign you in for some reason")
+				.SetPositiveButton("uh", (EventHandler<DialogClickEventArgs>)null)
+				.Show();
+		}
+	}
+}
--- CubeQuest/MainActivity.cs
+++ CubeQuest/MainActivity.cs
@@ -1,17 +1,45 @@
 ﻿using Android.App;
+using Android.Content;
+using Android.Gms.Common;
 using Android.OS;
 using Android.Support.V7.App;
 
 namespace CubeQuest
 {
-	[Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private AccountManager signInManager;
+
+        private const int RcSignIn = 9001;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
+
+            // Google signin
+            signInManager = new AccountManager(this);
+
+			// Sign in button
+	        var signInButton = FindViewById<SignInButton>(Resource.Id.button_sign_in);
+			signInButton.SetColorScheme(SignInButton.ColorDark);
+			signInButton.SetSize(SignInButton.SizeWide);
+
+            signInButton.Click += (sender, args) =>
+            {
+                var intent = signInManager.GetSignInIntent();
+                StartActivityForResult(intent, RcSignIn);
+            };
         }
-	}
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+
+            if (requestCode == RcSignIn)
+                signInManager.HandleResult(data);
+        }
+    }
 }
 
