3ed0d582dfc1319708b5dbd80a9292e8cddfe7ca
kraxarn <6675728+kraxarn@users.noreply.github.com>
2018-12-10 00:39:09 +0100

    Add basic map

---
+++ CubeQuest/GameActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace CubeQuest
+{
+    [Activity(Label = "GameActivity")]
+    public class GameActivity : AppCompatActivity, IOnMapReadyCallback
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_game);
+
+            // Get map and listen when it's ready
+            var mapFragment = (SupportMapFragment) SupportFragmentManager.FindFragmentById(Resource.Id.map);
+            mapFragment.GetMapAsync(this);
+        }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            var defaultPosition = new LatLng(59.618706, 16.540438);
+            googleMap.MoveCamera(CameraUpdateFactory.NewLatLng(defaultPosition));
+        }
+    }
+}
