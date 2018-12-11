c354c66b54cd4abb2692505c2a536dfb02aa8df8
kraxarn <me@kraxarn.com>
2018-12-11 01:02:48 +0100

    Add some comments and stuffs

--- CubeQuest/Account/User.cs
+++ CubeQuest/Account/User.cs
@@ -6,21 +6,40 @@ namespace CubeQuest.Account
 {
     public class User
     {
+        /// <summary>
+        /// Primary weapon equipped, like a sword
+        /// </summary>
         private IWeapon primaryWeapon;
+
+        /// <summary>
+        /// Secondary weapon equipped, like a shield
+        /// </summary>
         private IWeapon secondaryWeapon;
 
+        /// <summary>
+        /// Magic spell equipped
+        /// </summary>
         private ISpell spell;
 
+        /// <summary>
+        /// All companions the player has
+        /// </summary>
         private readonly List<ICompanion> companions;
 
-        public IEnumerable<ICompanion> Companions => companions;
+        /// <summary>
+        /// All companions currently equipped by the user (max 3)
+        /// </summary>
+        private readonly List<ICompanion> equippedCompanions;
 
+        /// <summary>
+        /// Level of the user, decides base stats - TODO
+        /// </summary>
         private uint level;
 
         public User()
         {
             companions = new List<ICompanion>();
-
+            equippedCompanions = new List<ICompanion>(3);
             level = 0;
         }
 
@@ -35,5 +54,15 @@ namespace CubeQuest.Account
         /// </summary>
         public static User FromString(string json) => 
             JSON.ToObject<User>(json);
+
+        public void Attack()
+        {
+            // TODO
+        }
+
+        public void Damage()
+        {
+            // TODO
+        }
     }
 }
