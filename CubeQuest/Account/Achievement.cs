71eb64932818b6bd7a3be6bca8f86f33e4e5e798
kraxarn <me@kraxarn.com>
2018-12-14 22:59:32 +0100

    Add achievement IDs

---
+++ CubeQuest/Account/Achievement.cs
@@ -0,0 +1,8 @@
+﻿namespace CubeQuest.Account
+{
+    public static class Achievement
+    {
+        public static string Test => "CgkIgqDY5-YMEAIQAQ";
+        public static string TestIncremental => "CgkIgqDY5-YMEAIQAg";
+    }
+}
