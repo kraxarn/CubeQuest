53a21629e675eb3d75c73ccb31cf76638a6e6261
kraxarn <me@kraxarn.com>
2018-12-16 16:58:11 +0100

    Add initial enemy interface

---
+++ CubeQuest/Account/Enemies/EnemySnake.cs
@@ -0,0 +1,19 @@
+﻿using CubeQuest.Account.Interface;
+
+namespace CubeQuest.Account.Enemies
+{
+    public class EnemySnake : IEnemy
+    {
+        public string Icon => "snake";
+
+        public string Name => "Danger Noodle";
+
+        public string Info => "A very dangerous noodle compared to regular ones";
+
+        public int Health => 10;
+
+        public int Armor => 0;
+
+        public int Attack => 5;
+    }
+}
