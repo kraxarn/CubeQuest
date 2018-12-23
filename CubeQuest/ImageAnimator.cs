141117b23db53af711b2d41ee48b3532a82085bd
kraxarn <6675728+kraxarn@users.noreply.github.com>
2018-12-23 23:38:35 +0100

    Add Done event and add ability to change frames

--- CubeQuest/ImageAnimator.cs
+++ CubeQuest/ImageAnimator.cs
@@ -8,33 +8,74 @@ namespace CubeQuest
 {
     public class ImageAnimator
     {
-        private bool running;
+	    public delegate void DoneEvent();
 
-        /// <summary>
-        /// Animate an <see cref="ImageView"/>
-        /// </summary>
-        /// <param name="image"><see cref="ImageView"/> to be animated</param>
-        /// <param name="bitmaps">All bitmaps in the animation</param>
-        /// <param name="delay">Delay in milliseconds</param>
-        public ImageAnimator(ImageView image, IReadOnlyList<Bitmap> bitmaps, int delay)
-        {
+		/// <summary>
+		/// When a non-repeating animation finishes
+		/// </summary>
+	    public event DoneEvent Done;
+		
+		private bool running;
+
+		private IReadOnlyList<Bitmap> bitmaps;
+
+		private int current;
+
+		private int delay;
+
+		private readonly ImageView image;
+
+		/// <summary>
+		/// Animate an <see cref="ImageView"/>
+		/// </summary>
+		/// <param name="image"><see cref="ImageView"/> to be animated</param>
+		/// <param name="bitmaps">All bitmaps in the animation</param>
+		/// <param name="delay">Delay in milliseconds</param>
+		public ImageAnimator(ImageView image, IReadOnlyList<Bitmap> bitmaps, int delay)
+		{
+			this.bitmaps = bitmaps;
+			this.delay   = delay;
+			this.image   = image;
+
             running = true;
-            var i = 0;
+            current = 0;
 
-            Task.Run(() =>
-            {
-                while (running)
-                {
-                    if (i == bitmaps.Count)
-                        i = 0;
-
-                    image.SetImageBitmap(bitmaps[i]);
-                    i++;
-                    Thread.Sleep(delay);
-                }
-            });
+            Task.Run(() => Loop());
         }
 
+		/// <summary>
+		/// Start a new animation on the same image
+		/// </summary>
+		public void New(IReadOnlyList<Bitmap> newBitmaps, int newDelay)
+		{
+			bitmaps = newBitmaps;
+			delay   = newDelay;
+
+			current = 0;
+
+			if (!running)
+				Task.Run(() => Loop());
+		}
+
+		private void Loop()
+		{
+			while (running)
+			{
+				if (current == bitmaps.Count)
+				{
+					Done?.Invoke();
+					current = 0;
+				}
+
+				image.SetImageBitmap(bitmaps[current]);
+				current++;
+				Thread.Sleep(delay);
+			}
+		}
+
+		/// <summary>
+		/// Stop the animation at the current frame
+		/// </summary>
         public void Stop() => running = false;
     }
 }
