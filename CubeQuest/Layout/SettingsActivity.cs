e4bbc0e98581c8106f5be50904c67572f155b407
kraxarn <me@kraxarn.com>
2018-12-30 22:28:17 +0100

    Add licenses dialog

--- CubeQuest/Layout/SettingsActivity.cs
+++ CubeQuest/Layout/SettingsActivity.cs
@@ -8,6 +8,8 @@ using Android.Support.V7.Widget;
 using Android.Util;
 using CubeQuest.ListView.Users;
 using System.Collections.Generic;
+using Android.Views;
+using Android.Widget;
 using AlertDialog = Android.Support.V7.App.AlertDialog;
 
 namespace CubeQuest.Layout
@@ -47,25 +49,38 @@ namespace CubeQuest.Layout
 
 			FindPreference("credits").PreferenceClick += (sender, args) => 
 				OpenCredits();
+
+			FindPreference("licenses").PreferenceClick += (sender, args) =>
+				OpenLicenses();
 		}
 
+		/// <summary>
+		/// Builds the user entries adapter with the view and entries provided
+		/// </summary>
+		private UserEntriesAdapter BuildUserEntriesAdapter(View view, IEnumerable<UserEntry> entries)
+		{
+			var viewUsers = view.FindViewById<RecyclerView>(Resource.Id.view_users);
+			var adapter = new UserEntriesAdapter(entries, Context.Assets);
+			viewUsers.SetAdapter(adapter);
+			viewUsers.SetLayoutManager(new LinearLayoutManager(Context));
+			viewUsers.HasFixedSize = true;
+
+			return adapter;
+		}
+
+		/// <summary>
+		/// Opens the credits dialog
+		/// </summary>
 		private void OpenCredits()
 		{
-			var developers = new List<UserEntry>
+			var view = LayoutInflater.Inflate(Resource.Layout.view_dialog_credits, null, false);
+			var adapter = BuildUserEntriesAdapter(view, new List<UserEntry>
 			{
 				new UserEntry("developer/dennizlund.webp", "dennizlund", "Designer"),
 				new UserEntry("developer/kraxarn.webp",    "kraxarn",    "Developer and designer"),
 				new UserEntry("developer/tacmotor.webp",   "tacmotor",   "Inventory designer"),
 				new UserEntry("developer/timnnyman.webp",  "TimNNyman",  "Enemy placement and battle system")
-			};
-
-			var view = LayoutInflater.Inflate(Resource.Layout.view_dialog_credits, null, false);
-
-			var viewUsers = view.FindViewById<RecyclerView>(Resource.Id.view_users);
-			var adapter = new UserEntriesAdapter(developers, Context.Assets);
-			viewUsers.SetAdapter(adapter);
-			viewUsers.SetLayoutManager(new LinearLayoutManager(Context));
-			viewUsers.HasFixedSize = true;
+			});
 			
 			adapter.OnItemClick += itemView =>
 				StartActivity(new Intent(Intent.ActionView)
@@ -75,6 +90,31 @@ namespace CubeQuest.Layout
 				.SetView(view)
 				.SetTitle("Credits")
 				.SetPositiveButton("OK", (IDialogInterfaceOnClickListener) null)
+				.Show();
+		}
+
+		/// <summary>
+		/// Opens the licenses dialog
+		/// </summary>
+		private void OpenLicenses()
+		{
+			var view = LayoutInflater.Inflate(Resource.Layout.view_dialog_credits, null, false);
+			view.FindViewById<TextView>(Resource.Id.text_special_thanks).Visibility = ViewStates.Gone;
+
+			var adapter = BuildUserEntriesAdapter(view, new List<UserEntry>
+			{
+				new UserEntry(null,  "fastJSON", "By mgholam (CPOL license)"),
+				new UserEntry(null,  "Xamarin",  "By Xamarin Inc. (MIT license)")
+			});
+
+			adapter.OnItemClick += itemView =>
+				StartActivity(new Intent(Intent.ActionView)
+					.SetData(Uri.Parse($"https://github.com/{(itemView.Title.Text == "fastJSON" ? "mgholam/fastJSON" : "xamarin/xamarin-android")}")));
+
+			new AlertDialog.Builder(Context)
+				.SetView(view)
+				.SetTitle("Open Source Licenses")
+				.SetPositiveButton("OK", (IDialogInterfaceOnClickListener)null)
 				.Show();
 		}
 	}
