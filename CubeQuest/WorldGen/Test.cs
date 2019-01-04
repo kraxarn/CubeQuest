1831dabce52cbb44d2edef0ac6afd3ffa0a7915a
Tim Nyman <tnn17004@student.mdh.se>
2019-01-04 11:29:48 +0100

    Added chunk loading system toghether with load monster position

---
+++ CubeQuest/WorldGen/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Gms.Maps.Model;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace CubeQuest.WorldGen
+{
+    class Test
+    {
+        public static void generateTestMarker(LatLng coord)
+        {
+            MapHandler.AddMarker(coord, "Title", BitmapDescriptorFactory.FromAsset("enemy/snake.webp"));
+        }
+    }
+}
