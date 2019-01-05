e0fb9fba9cfac03be2c0a92c1e1e8d9c311005d1
kraxarn <me@kraxarn.com>
2019-01-05 16:23:17 +0100

    Add auto end

--- CubeQuest/Battle/QueueAction.cs
+++ CubeQuest/Battle/QueueAction.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace CubeQuest.Battle
 {
@@ -10,12 +11,33 @@ namespace CubeQuest.Battle
 		private readonly Action action;
 		
 		public event EventHandler OnEnd;
-		
-		public QueueAction(Action executeAction) => 
-			action = executeAction;
 
-		public void Execute() => 
-			action.Invoke();
+		private readonly bool autoEnd;
+		
+		/// <summary>
+		/// Generic class for a <see cref="BattleQueue"/> item
+		/// </summary>
+		/// <param name="executeAction">Action to be executed</param>
+		/// <param name="autoEnd">Start a new thread and call <see cref="End()"/> when finished</param>
+		public QueueAction(Action executeAction, bool autoEnd = false)
+		{
+			action = executeAction;
+			this.autoEnd = autoEnd;
+		}
+
+		public void Execute()
+		{
+			if (autoEnd)
+			{
+				Task.Run(() =>
+				{
+					action.Invoke();
+					End();
+				});
+			}
+			else
+				action.Invoke();
+		}
 
 		public void End() => 
 			OnEnd?.Invoke(this, null);
