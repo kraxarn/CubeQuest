a9b50f12e18e15452631be0ab5eb59f3161dbbd0
kraxarn <me@kraxarn.com>
2019-01-05 14:41:52 +0100

    Add battle queue

---
+++ CubeQuest/Battle/IQueueAction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CubeQuest.Battle
+{
+	public interface IQueueAction
+	{
+		/// <summary>
+		/// When the item is done with it's thing
+		/// </summary>
+		event EventHandler OnEnd;
+
+		/// <summary>
+		/// The action to execute when its next in line
+		/// </summary>
+		void Execute();
+	}
+}
