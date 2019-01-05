2e237710a9d036e9bb69f16cc86015355c17de4d
kraxarn <me@kraxarn.com>
2019-01-05 15:13:09 +0100

    Add generic queue action

---
+++ CubeQuest/Battle/QueueAction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CubeQuest.Battle
+{
+	/// <summary>
+	/// Generic <see cref="IQueueAction"/>
+	/// </summary>
+	public class QueueAction : IQueueAction
+	{
+		private readonly Action action;
+		
+		public event EventHandler OnEnd;
+		
+		public QueueAction(Action executeAction) => 
+			action = executeAction;
+
+		public void Execute() => 
+			action.Invoke();
+	}
+}
