62b3d590ea09087393c998807670f096c846bbf5
Tim Nyman <Tim.N.Nyman@gmail.com>
2019-01-06 17:02:56 +0100

    Created enemy factory

--- CubeQuest/WorldGen/Chunk.cs
+++ CubeQuest/WorldGen/Chunk.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using Android.Gms.Maps.Model;
 using Android.Util;
+using CubeQuest.Account.Interface;
+using CubeQuest.MonsterGen;
 
 namespace CubeQuest.WorldGen
 {
@@ -25,8 +27,8 @@ namespace CubeQuest.WorldGen
             for (int i = 0; i < points.Count; i++)
             {
                 double val = points[i].Value;
-                //Value: 
-                Markers.Add(MapHandler.AddMarker(points[i].ToLatLng(), "Title", BitmapDescriptorFactory.FromAsset("enemy/snake.webp")));
+                IEnemy enemy = MonsterFactory.CreateMonster(val);
+                Markers.Add(MapHandler.AddMarker(points[i].ToLatLng(), enemy.Name, BitmapDescriptorFactory.FromAsset(enemy.Icon)));
                 Markers[i].Tag = val;
             }
         }
