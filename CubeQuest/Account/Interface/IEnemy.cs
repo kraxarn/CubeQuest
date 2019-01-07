6cf40e9de867d1695a02c381c6b7c6a2340bd6dd
kraxarn <me@kraxarn.com>
2019-01-07 16:15:55 +0100

    Add methods to convert IEnemy to json

--- CubeQuest/Account/Interface/IEnemy.cs
+++ CubeQuest/Account/Interface/IEnemy.cs
@@ -1,9 +1,20 @@
 ﻿using CubeQuest.Handler;
+using fastJSON;
+using Java.Lang;
 
 namespace CubeQuest.Account.Interface
 {
     public interface IEnemy : IItem
     {
 	    ImageHandler.ImageName Image { get; }
+    }
+
+    public static class EnemyAsJavaString
+    {
+	    public static String ToJavaString(this IEnemy enemy) => 
+		    new String(JSON.ToJSON(enemy));
+
+	    public static IEnemy CreateEnemyFromJson(this Object json) => 
+		    JSON.ToObject(json.ToString()) as IEnemy;
     }
 }
