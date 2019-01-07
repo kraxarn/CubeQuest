a7557f2e3e77b7086e3f7309f950246b35a8cc74
kraxarn <me@kraxarn.com>
2019-01-07 16:59:10 +0100

    Add companion manager

---
+++ CubeQuest/Handler/CompanionManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CubeQuest.Account.Companions;
+using CubeQuest.Account.Interface;
+
+namespace CubeQuest.Handler
+{
+	public static class CompanionManager
+	{
+		private static readonly Type[] Companions =
+		{
+			typeof(Bear),
+			typeof(Buffalo),
+			typeof(Chick),
+			typeof(Horse),
+			typeof(Owl),
+			typeof(Parrot),
+			typeof(Penguin),
+			typeof(Rabbit),
+			typeof(Sloth),
+			typeof(Snake),
+			typeof(Walrus),
+			typeof(Wolf)
+		};
+
+		/// <summary>
+		/// Get a random companion
+		/// </summary>
+		public static ICompanion GetRandom() => 
+			Activator.CreateInstance(Companions[new Random().Next(Companions.Length)]) as ICompanion;
+	}
+}
