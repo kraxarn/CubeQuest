1565b280207a0dc17b336ef318d686ae36c48c18
Dennis Lund <denniz_lund@hotmail.com>
2019-01-08 10:32:01 +0100

    Added narwhale and unique starting companions

--- CubeQuest/Handler/CompanionManager.cs
+++ CubeQuest/Handler/CompanionManager.cs
@@ -12,6 +12,7 @@ namespace CubeQuest.Handler
 			typeof(Buffalo),
 			typeof(Chick),
 			typeof(Horse),
+            typeof(Narwhale),
 			typeof(Owl),
 			typeof(Parrot),
 			typeof(Penguin),
@@ -27,5 +28,20 @@ namespace CubeQuest.Handler
 		/// </summary>
 		public static ICompanion GetRandom() => 
 			Activator.CreateInstance(Companions[new Random().Next(Companions.Length)]) as ICompanion;
+
+        public static ICompanion[] GetStartingCompanions()
+        {
+            var seed = new Random().Next(Companions.Length);
+            var interval = new Random().Next(1,12);
+
+            var list = new ICompanion[3];
+
+            for (var i = 0; i < 3; i++)
+            {
+                list[i] = Activator.CreateInstance(Companions[(seed + (interval * i)) % Companions.Length]) as ICompanion;
+            }
+
+            return list;
+        }
 	}
 }
