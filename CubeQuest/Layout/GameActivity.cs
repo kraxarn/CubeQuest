9eb85f05a149306dfed497686bfb9176d4f02140
Tim Nyman <Tim.N.Nyman@gmail.com>
2019-01-08 14:38:47 +0100

    Changed the date format that is used for making the seed

--- CubeQuest/Layout/GameActivity.cs
+++ CubeQuest/Layout/GameActivity.cs
@@ -292,7 +292,7 @@ namespace CubeQuest.Layout
         {
             // Set local maps
             googleMap = map;
-            MapHandler.Init(map, DateTime.Now.ToString("M/d/yyyy").GetHashCode());
+            MapHandler.Init(map, DateTime.Now.ToString("dd/MM/yyyy").GetHashCode());
 
             map.CameraChange += Map_CameraChange;
 
